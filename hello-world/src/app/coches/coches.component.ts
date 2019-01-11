b8ce3d8e21c84202f685902002aa24c8c21835fe
Jael Alexander Rivera Oviedo <jrivera@koghi.com>
2019-01-11 10:03:21 -0500

    Validacion de formularios

--- hello-world/src/app/coches/coches.component.ts
+++ hello-world/src/app/coches/coches.component.ts
@@ -8,13 +8,19 @@ import {Coche} from './coche';
 export class CochesCompoent{
 
     private coche: Coche;
-
+    private listCoches :Array<Coche>;
     constructor(){
         this.coche= new Coche(null,null,null);
+        this.listCoches=[
+            new Coche("panda","10","Wolsvagen"),
+            new Coche("panda1","11","Wolsvagen1"),
+            new Coche("panda2","12","Wolsvagen2"),
+        ]
     }
 
 
     onSubmit(){
         console.log(this.coche);
+        this.listCoches.push(this.coche);
     }
 }
